06f2142747df281e86276add30d36ce85b86aa0d
Adnan Rasim <54524021+userado@users.noreply.github.com>
2021-03-18 13:11:52 +0200

    Update Science.cpp

--- Science.cpp
+++ Science.cpp
@@ -1,43 +1,43 @@
-﻿#include "Science.h"
+#include "Science.h"
 #include <fstream> //file operations
 ofstream science; //file operations
 
 void Science::Exp2() {
 	resExp2 = exp2(valExp2);
-	cout << defpar << "Exp2 (Пр. 4.50): "; cin >> valExp2;
-	cout << "Експонентната стойност от" << valExp2 << " е " << resExp2 << endl;
+	cout << defpar << "Exp2 (Ex. 4.50): "; cin >> valExp2;
+	cout << "Exponential value of" << valExp2 << " is " << resExp2 << endl;
 
 	//saving data to new file
 	science.open("JetCalc_App_Data/Exp2_Data/Data.txt");//open file
-	science << "Това са вашите данни от JetCalc операция Exp2!" << endl << endl;
+	science << "This is your JetCalc Exp2 calculation data!" << endl << endl;
 	science << defpar << "Exp2: " << valExp << endl;
-	science << "Експонентната стойност от" << valExp2 << " е " << resExp2 << endl;
+	science << "Exponential value of" << valExp2 << " is " << resExp2 << endl;
 	science.close();//close file
 }
 
 void Science::Exp() {
 	resExp = exp(valExp);
-	cout << defpar << "Exp (Пр. 2.00): "; cin >> valExp;
-	cout << "Експонентната стойност от " << valExp << " е " << resExp << endl;
+	cout << defpar << "Exp (Ex. 2.00): "; cin >> valExp;
+	cout << "Exponential valie of " << valExp << " is " << resExp << endl;
 
 	//saving data to new file
 	science.open("JetCalc_App_Data/Exp_Data/Data.txt");//open file
-	science << "Това са вашите данни от JetCalc операция Exp!" << endl << endl;
+	science << "This is your JetCalc Exp calculation data!" << endl << endl;
 	science << defpar << "Exp: " << valExp << endl;
-	science << "Експонентната стойност от " << valExp << " е " << resExp << endl;
+	science << "Exponential valie of " << valExp << " is " << resExp << endl;
 	science.close();//close file
 }
 
 void Science::Log() {
 	resLog = log(valLog);
-	cout << defpar << "Log (Пр. 5.00): "; cin >> valLog;
-	cout << "Логаритъм от " << valLog << " е " << resLog << endl;
+	cout << defpar << "Log (Ex. 5.00): "; cin >> valLog;
+	cout << "Logarithm of " << valLog << " is " << resLog << endl;
 
 	//saving data to new file
 	science.open("JetCalc_App_Data/Logarithm_Data/Data.txt");//open file
-	science << "Това са вашите данни от JetCalc операция изчисление на логаритъм!" << endl << endl;
+	science << "This is your JetCalc Logarithm calculation data!" << endl << endl;
 	science << defpar << "Log: " << valLog << endl;
-	science << "Логаритъм от " << valLog << " е " << resLog << endl;
+	science << "Logarithm of " << valLog << " is " << resLog << endl;
 	science.close();//close file
 }
 
@@ -45,14 +45,14 @@ void Science::Cureent() {
 	cout << defpar << "U (double): "; cin >> U; cout << endl;
 	cout << defpar << "R (double): "; cin >> R; cout << endl;
 	I = U / R;
-	cout << "Резултат: I = " << I << " Amp" << endl << endl;
+	cout << "Result: I = " << I << " Amp" << endl << endl;
 
 	//saving data to new file
 	science.open("JetCalc_App_Data/Current_Data/Data.txt");//open file
-	science << "Това са вашите данни от JetCalc операция изчисление на електричество!" << endl << endl;
+	science << "This is your JetCalc current calculation data!" << endl << endl;
 	science << defpar << "U (double): " << U << endl;
 	science << defpar << "R (double): " << R << endl;
-	science << "Резултат: I = " << I << " Amp";
+	science << "Result: I = " << I << " Amp";
 	science.close();//close file
 }
 
@@ -60,12 +60,12 @@ void Science::Resistance() {
 	cout << defpar << "U (double): "; cin >> U; cout << endl;
 	cout << defpar << "I (double): "; cin >> I; cout << endl;
 	R = U / I;
-	cout << "Резултат: R = " << R << " Ohms" << endl << endl;
+	cout << "Result: R = " << R << " Ohms" << endl << endl;
 }
 
 void Science::Voltage() {
 	cout << defpar << "I (double): "; cin >> I; cout << endl;
 	cout << defpar << "R (double): "; cin >> R; cout << endl;
 	U = I * R;
-	cout << "Резултат: U = " << U << " Volts" << endl << endl;
+	cout << "Result: U = " << U << " Volts" << endl << endl;
 }
