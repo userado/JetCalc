48a1d70746d7e0d6a212b7a66b996f625008f8c3
Adnan Rasim <54524021+userado@users.noreply.github.com>
2021-03-18 12:45:35 +0200

    Update Welcome.cpp

--- Welcome.cpp
+++ Welcome.cpp
@@ -4,18 +4,18 @@ ofstream welcomeuser; //file operations
 
 void Welcome::VersionInfo()
 {
-	cout << " Добре дошли в " << softname << endl;
-	cout << " Версия: " << softversion << endl;
+	cout << " Welcome to " << softname << endl;
+	cout << " Version: " << softversion << endl;
 	cout << endl;
 }
 
 void Welcome::Credits() {
-	cout << " Този продукт е създаден от " << owner << endl;
-	cout << " Телефон: +359897228344" << endl;
-	cout << " KEYBASE: " << keybase << endl;
-	cout << " Ел. Поща: " << email << endl;
+	cout << " This program is developed by " << owner << endl;
+	cout << " Phone: +359897228344" << endl;
+	cout << " Keybse: " << keybase << endl;
+	cout << " E-mail: " << email << endl;
 	cout << endl;
 }
 void Welcome::Manual() {
-	system("xdg-open https://keybase.pub/neongreen/software/Linux/JetCalc/v_1_0_3/readme/");
+	system("xdg-open https://github.com/userado/JetCalc/blob/main/README.md");
 }
